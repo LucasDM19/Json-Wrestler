5d3f6d47e19dfaffbb21d839f82276cfd9385440
LucasDM19 <lucasmuzel@gmail.com>
2018-08-18 15:38:00 -0300

    Parte 2
    Adicionei jquery. Agora ele captura o primeiro link da página, e a joga na console. JavaScript.

--- js/content.js
+++ js/content.js
@@ -1,2 +1,5 @@
 //Teste de mensagem
-alert("Ei, aqui eh a extensao do Chrome extension! QAP?")
+//alert("Ei, aqui eh a extensao do Chrome extension! QAP?")
+
+var firstHref = $("a[href^='http']").eq(0).attr("href"); //log the URL of the first external link on the page
+console.log(firstHref);
